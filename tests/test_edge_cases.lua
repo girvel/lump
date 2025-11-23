d942fa64ae9a6a71b735c3bc16974766a847e6c0
girvel <widauka@ya.ru>
2025-11-23 22:36:07 +0500

    A bit of test renaming

--- tests/test_edge_cases.lua
+++ tests/test_edge_cases.lua
@@ -1,14 +1,14 @@
 local fw = require("tests.fw")
 
 
-fw.test("math.huge", function()
+fw.test("pass: math.huge", function()
   fw.pass(math.huge)
 end)
 
-fw.test("-math.huge", function()
+fw.test("pass: -math.huge", function()
   fw.pass(-math.huge)
 end)
 
-fw.test("nan", function()
+fw.test("pass: nan", function()
   fw.pass(0/0)
 end)
